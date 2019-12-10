71ff8a5431a8ca64f593248254bdbd9da31a0e70
So0ni <173976914@qq.com>
2019-12-10 12:06:35 +0800

    config update

--- scripts/imageFilter.js
+++ scripts/imageFilter.js
@@ -6,27 +6,26 @@ function getPosition(str, m, i) {
     return str.split(m, i).join(m).length;
 }
 
-hexo.extend.filter.register('after_post_render', function(data){
+hexo.extend.filter.register('after_post_render', function (data) {
     var config = hexo.config;
-    if(config.post_asset_folder){
+    if (config.post_asset_folder) {
         var link = data.permalink;
         var beginPos = getPosition(link, '/', 3) + 1;
         var appendLink = '';
         // In hexo 3.1.1, the permalink of "about" page is like ".../about/index.html".
         // if not with index.html endpos = link.lastIndexOf('.') + 1 support hexo-abbrlink
-        if(/.*\/index\.html$/.test(link)) {
+        if (/.*\/index\.html$/.test(link)) {
             // when permalink is end with index.html, for example 2019/02/20/xxtitle/index.html
             // image in xxtitle/ will go to xxtitle/index/
             appendLink = 'index/';
             var endPos = link.lastIndexOf('/');
-        }
-        else {
-            var endPos = link.length-1;
+        } else {
+            var endPos = link.length - 1;
         }
         link = link.substring(beginPos, endPos) + '/' + appendLink;
 
         var toprocess = ['excerpt', 'more', 'content'];
-        for(var i = 0; i < toprocess.length; i++){
+        for (var i = 0; i < toprocess.length; i++) {
             var key = toprocess[i];
 
             var $ = cheerio.load(data[key], {
@@ -36,31 +35,31 @@ hexo.extend.filter.register('after_post_render', function(data){
                 decodeEntities: false
             });
 
-            $('img').each(function(){
-                if ($(this).attr('src')){
+            $('img').each(function () {
+                if ($(this).attr('src')) {
                     // For windows style path, we replace '\' to '/'.
                     var src = $(this).attr('src').replace('\\', '/');
-                    if(!(/http[s]*.*|\/\/.*/.test(src)
+                    if (!(/http[s]*.*|\/\/.*/.test(src)
                         || /^\s+\//.test(src)
                         || /^\s*\/uploads|images\//.test(src))) {
                         // For "about" page, the first part of "src" can't be removed.
                         // In addition, to support multi-level local directory.
-                        var linkArray = link.split('/').filter(function(elem){
+                        var linkArray = link.split('/').filter(function (elem) {
                             return elem != '';
                         });
-                        var srcArray = src.split('/').filter(function(elem){
+                        var srcArray = src.split('/').filter(function (elem) {
                             return elem != '' && elem != '.';
                         });
-                        if(srcArray.length > 1)
+                        if (srcArray.length > 1)
                             srcArray.shift();
                         src = srcArray.join('/');
-
-                        $(this).attr('src', config.root + link + src);
-                        console.info&&console.info("update link as:-->"+config.root + link + src);
+                        var imgSrc = (config.cdn_url ? config.cdn_url : '') + config.root + (config.branch ? config.branch + '/' : '') + link + src.split('/').pop();
+                        $(this).attr('src', imgSrc);
+                        console.info && console.info("update link as:-->" + imgSrc);
                     }
-                }else{
-                    console.info&&console.info("no src attr, skipped...");
-                    console.info&&console.info($(this));
+                } else {
+                    console.info && console.info("no src attr, skipped...");
+                    console.info && console.info($(this));
                 }
             });
             data[key] = $.html();
